5068a7fe9e55a813271718b311aeb4bd537e3bb2
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2022-09-02 01:29:02 +0200

    Web: Initial commit

---
+++ web/svelte.config.js
@@ -0,0 +1,29 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
+
+	kit: {
+		adapter: adapter(),
+		files: {
+			assets: 'static',
+			routes: 'src/routes',
+			appTemplate: 'src/app.html'
+		},
+		moduleExtensions: ['.ts'],
+		paths: {
+			assets: '',
+			base: ''
+		}
+	}
+};
+
+export default config;
